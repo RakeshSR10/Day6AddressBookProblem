94f25a4f70bb6d15f4f6a2831f2df9256bf7f318
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-05 22:29:41 +0530

    [Modified] added Contacts in file AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -1,1 +1,14 @@
 Welcome to Addree Book Computation Program
+public class AddressBook 
+{
+	public static void main(String[] args) 
+	{
+		String first_name="Rakesh";
+		String last_name="S R";
+		String city="Davanagere";
+		String state="Karnataka";
+		int ph_number=12347596;
+		int zip=577001;
+		String email="abc@gmail.com";
+	}
+}
